10d667e9c0e4cbb66dffb4bb6524561ae227cdf7
Dubzer <me@dubzer.dev>
2023-09-30 00:19:03 +0300

    Refactoring

---
+++ src/handler_error.rs
@@ -0,0 +1,26 @@
+use std::fmt::Display;
+use hyper::{http::HeaderValue, StatusCode};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub struct HandlerError {
+    pub message: String,
+    pub code: StatusCode,
+    pub origin: Option<HeaderValue>,
+}
+
+impl Display for HandlerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl HandlerError {
+    pub fn new(message: impl Into<String>, code: StatusCode) -> Self {
+        HandlerError { message: message.into(), code, origin: None }
+    }
+
+    pub fn new_with_origin(message: impl Into<String>, code: StatusCode, origin: HeaderValue) -> Self {
+        HandlerError { message: message.into(), code, origin: Some(origin) }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod handler_error;
+
 use std::env;
 use std::str::FromStr;
 use hyper::http::HeaderValue;
@@ -6,44 +8,38 @@ use hyper::{Body, Request, Response, Server, StatusCode, Client, HeaderMap, Meth
 use hyper::http::uri::{Authority, Scheme};
 use hyper_tls::HttpsConnector;
 use once_cell::sync::Lazy;
+use crate::handler_error::HandlerError;
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, GenericError>;
 
 static SECRET: Lazy<Option<String>> = Lazy::new(|| env::var("SECRET").ok());
 
+
 async fn handle(mut req: Request<Body>) -> Result<Response<Body>> {
-    let Some(destination_header) = req.headers_mut().remove("silly-host") else {
-        return Ok(validation_error("can i hav some of that Silly-host header, pwease? 🥺", None));
-    };
+    let _smth: u8 = "asd".parse()?;
+    let origin = req.headers().get("Origin")
+        .ok_or(HandlerError::new("can i hav some of that origin header, pwease? 🥺", StatusCode::BAD_REQUEST))?.clone();
 
-    let Some(origin) = req.headers().get("Origin") else {
-        return Ok(validation_error("can i hav some of that origin header, pwease? 🥺", None));
-    };
+    let destination_header = req.headers_mut().remove("silly-host")
+        .ok_or(HandlerError::new_with_origin("can i hav some of that Silly-host header, pwease? 🥺", StatusCode::BAD_REQUEST, origin.clone()))?;
 
-    let origin = origin.clone();
-
-    let Ok(authority) = Authority::from_str(destination_header.to_str().unwrap()) else {
-        return Ok(validation_error("your Silly-host header looks like an invalid d  omain 🥺", Some(&origin)))
-    };
+    let authority = Authority::from_str(destination_header.to_str().unwrap())
+        .map_err(|_| HandlerError::new_with_origin("your Silly-host header looks like an invalid domain 🥺", StatusCode::BAD_REQUEST, origin.clone()))?;
 
     let mut uri_parts = req.uri().clone().into_parts();
     uri_parts.authority = Some(authority);
     uri_parts.scheme = Some(Scheme::HTTPS);
 
-    *req.headers_mut().get_mut("Host").unwrap() = destination_header;
-    *req.uri_mut() = Uri::from_parts(uri_parts).unwrap();
+    req.headers_mut().insert("Host", destination_header);
+    *req.uri_mut() = Uri::from_parts(uri_parts)?;
 
     let client = Client::builder().build(HttpsConnector::new());
-
-    let client_response = match client.request(req).await {
-        Ok(result) => result,
-        Err(err) => return Ok(validation_error(format!("oops, couldn't connect to destination :(\n{}", err).as_str(), Some(&origin)))
-    };
+    let client_response = client.request(req).await
+        .map_err(|err| HandlerError::new_with_origin(format!("oops, couldn't connect to destination :(\n{}", err), StatusCode::INTERNAL_SERVER_ERROR, origin.clone()))?;
     
     let (mut parts, body) = client_response.into_parts();
-
-    parts.headers.extend(get_default_cors(&origin));
+    parts.headers.extend(get_default_cors(origin.clone()));
 
     return Ok(Response::from_parts(parts, body));
 }
@@ -59,7 +55,7 @@ async fn handle_options(req: Request<Body>) -> Result<Response<Body>> {
 
     let mut response = Response::builder().status(StatusCode::OK);
 
-    let mut headers = get_default_cors(origin);    
+    let mut headers = get_default_cors(origin.clone());    
     headers.insert("Access-Control-Allow-Headers", HeaderValue::from_static("*"));
 
     response.headers_mut().unwrap().extend(headers);
@@ -67,27 +63,40 @@ async fn handle_options(req: Request<Body>) -> Result<Response<Body>> {
 }
 
 async fn route(req: Request<Body>) -> Result<Response<Body>> {
+    #[cfg(debug_assertions)]
     println!("req: {:?}", req);
-    match (req.method(), req.uri().path()) {
+    
+    let result = match (req.method(), req.uri().path()) {
         (&Method::OPTIONS, _) => handle_options(req).await,
         _ => handle(req).await
-    }
+    };
+
+    Ok(result.unwrap_or_else(|err| {
+        match err.downcast::<HandlerError>() {
+            Ok(err) => {
+                let mut response = Response::builder().status(err.code);
+                if let Some(origin) = err.origin {
+                    let headers = get_default_cors(origin);    
+                    response.headers_mut().unwrap().extend(headers);    
+                }
+            
+                response.body(Body::from(format!("Silly error: {}", err.message))).unwrap()
+            }
+            err @ Err(_) => {
+                println!("err: {:?}", &err.err().unwrap());
+
+                Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::from("Silly error: something bad happened 🥺. check the logs")).unwrap()
+            }
+        }
+    }))
 }
 
-fn validation_error(message: &str, origin: Option<&HeaderValue>) -> Response<Body> {
-    let mut response = Response::builder().status(StatusCode::BAD_REQUEST);
-    if let Some(origin) = origin {
-        let headers = get_default_cors(origin);    
-        response.headers_mut().unwrap().extend(headers);    
-    }
-
-    return response.body(Body::from(message.to_string())).unwrap()
-}
-
-fn get_default_cors(origin: &HeaderValue) -> HeaderMap {
+fn get_default_cors(origin: HeaderValue) -> HeaderMap {
     let mut cors_headers = HeaderMap::new();
     cors_headers.insert("Access-Control-Allow-Credentials", HeaderValue::from_static("true"));
-    cors_headers.insert("Access-Control-Allow-Origin", origin.clone());
+    cors_headers.insert("Access-Control-Allow-Origin", origin);
     cors_headers.insert("Access-Control-Allow-Methods", HeaderValue::from_static("GET, PUT, POST, DELETE, HEAD, PATCH, OPTIONS"));
     return cors_headers;
 }
